ebd8de12c62805839730b0142ae756a7ded65bc8
rahnakesume <rahnakesume@gmail.com>
2025-07-02 09:22:06 +0700

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
-# viraladik
-viraladik
+# [viraladik](https://viraladik.pages.dev)
---
+++ src/utils/site.ts
@@ -0,0 +1,4 @@
+export const nama = 'Viral Adik';
+export const desk = 'Video Bokep Indo Bocil Ngentot Jilbab Adik Montok Smp Mama Live Viral Colmek Sma Hijab Tante China';
+export const url = 'https://viraladik.pages.dev';
+export const terbit = '2025-01-26T00:01:03Z';
